0ea2b4d660953e379adfb287b0e80dbe5296732f
LL <1285615894@qq.com>
2020-04-03 22:13:04 +0800

    Header files and size

---
+++ README.md
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <windows.h>
+#include <time.h>
+
+#define L   30  // 游戏画面尺寸
+#define W 60
---
+++ game.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <windows.h>
+#include <time.h>
+
+#define L 30  // ”Œœ∑ª≠√Ê≥ﬂ¥Á
+#define W 60
