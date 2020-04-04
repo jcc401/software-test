e64979c3b4e8653e9bd6da995d6ae3644b16b5e0
LL <1285615894@qq.com>
2020-04-04 17:44:19 +0800

    startup and show

--- game.cpp
+++ game.cpp
@@ -20,3 +20,30 @@ void gotoxy(int x,int y)//类似于清屏函数
     pos.Y = y;
     SetConsoleCursorPosition(handle,pos);
 }
+void startup() // 数据初始化
+{
+	int i,j;
+	for (i=0;i<L;i++) // 初始化
+		for (j=0;j<W;j++)
+		{
+			cells[i][j] = 1;
+		}
+}
+
+void show()  // 显示画面
+{
+	gotoxy(0,0);  // 清屏
+	int i,j;
+	for (i=1;i<=L-1;i++)
+	{
+		for (j=1;j<=W-1;j++)
+		{
+			if (cells[i][j]==1)
+				printf("●");  //   输出活的细胞
+			else
+				printf("○");  //   输出空格			
+		}
+		printf("\n");
+	}
+	Sleep(500);
+}	
